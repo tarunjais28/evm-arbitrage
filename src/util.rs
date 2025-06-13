d06239a55fc862af3981a4f26f3abe644d369019
Tarun Jaiswal <tarunjais28@gmail.com>
2025-06-13 12:22:26 +0530

    Removed warnings

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,5 @@
 pub fn format_with_decimals(value: u128) -> String {
     let int_part = value / 1_000_000_000_000_000_000u128;
     let frac_part = value % 1_000_000_000_000_000_000u128;
-    format!("{}.{}", int_part, format!("{:018}", frac_part))
+    format!("{}.{:018}", int_part, frac_part)
 }
