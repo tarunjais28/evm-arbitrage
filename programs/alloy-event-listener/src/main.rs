441d2fdf69e7e103321204704ca35b02cf641a4a
Tarun Jaiswal <tarunjais28@gmail.com>
2025-06-26 18:14:27 +0530

    Added alloy-event-listener

---
+++ programs/alloy-event-listener/src/main.rs
@@ -0,0 +1,74 @@
+// use alloy_network::Ethereum;
+use alloy::{
+    primitives::address,
+    providers::{Provider, ProviderBuilder, WsConnect},
+    rpc::types::Filter,
+    sol,
+};
+use futures_util::stream::StreamExt;
+use utils::EnvParser;
+
+sol! {
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+
+    event Sync(
+        uint112 reserve0,
+        uint112 reserve1,
+    );
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    // Load environment variables from .env file
+    let env_parser = EnvParser::new()?;
+
+    // Set up the WS transport and connect.
+    let ws = WsConnect::new(env_parser.ws_address);
+    let provider = ProviderBuilder::new().connect_ws(ws).await?;
+
+    // Create a filter for the Swap event.
+    let filter = provider
+        .subscribe_logs(
+            &Filter::new().address(address!("0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852")),
+        )
+        .await?;
+    let mut stream = filter.into_stream();
+
+    println!("Waiting for swap events...");
+
+    // Process events from the stream.
+    while let Some(log) = stream.next().await {
+        if let Ok(decoded) = log.log_decode() {
+            let swap: Swap = decoded.inner.data;
+            println!("New Swap!");
+            println!("  TxHash: {:?}", log.transaction_hash);
+            println!("  BlockNumber: {}", log.block_number.unwrap_or_default());
+            println!("  PoolAddress: {}", decoded.inner.address);
+            println!("  Sender: {}", swap.sender);
+            println!("  To: {}", swap.to);
+            println!("  Amount 0 In: {}", swap.amount0In);
+            println!("  Amount 1 In: {}", swap.amount1In);
+            println!("  Amount 0 Out: {}", swap.amount0Out);
+            println!("  Amount 1 Out: {}", swap.amount1Out);
+            println!("---------------------------------");
+        } else if let Ok(decoded) = log.log_decode() {
+            let sync: Sync = decoded.inner.data;
+            println!("New Sync!");
+            println!("  TxHash: {:?}", log.transaction_hash);
+            println!("  BlockNumber: {}", log.block_number.unwrap_or_default());
+            println!("  PoolAddress: {}", decoded.inner.address);
+            println!("  Reserve 0: {}", sync.reserve0);
+            println!("  Reserve 1: {}", sync.reserve1);
+            println!("---------------------------------");
+        }
+    }
+
+    Ok(())
+}
