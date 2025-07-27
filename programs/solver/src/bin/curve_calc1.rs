f04157639065fa0521f83d567e88b31364d51155
Tarun Jaiswal <tarunjais28@gmail.com>
2025-07-28 01:02:57 +0530

    Fixed calculation

--- programs/solver/src/bin/curve_calc1.rs
+++ programs/solver/src/bin/curve_calc1.rs
@@ -23,6 +23,13 @@ sol!(
     "../../resources/contracts/erc20_abi.json"
 );
 
+sol!(
+    #[sol(rpc)]
+    #[derive(Debug)]
+    CurveContract,
+    "../../resources/contracts/curve_contract.json"
+);
+
 // source: https://github.com/curvefi/curve-contract/blob/master/contracts/pools/3pool/StableSwap3Pool.vy
 
 fn count_digits(mut n: BigInt) -> u32 {
@@ -80,41 +87,43 @@ pub async fn get_pool_data(
         precisions.push(BigInt::from(10u64.pow(u32::from(p))));
     }
 
-    let rates = [
-        BigInt::from(1000000000000000000u128),
-        BigInt::from(1000000000000000000000000000000u128),
-        BigInt::from(1000000000000000000000000000000u128),
-    ];
-    for rate in rates {
-        println!("rate: {rate}, digits: {}", count_digits(rate));
-    }
+    let curve_lp_token = address!("0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490");
+    let curve_contract = CurveContract::new(curve_lp_token, provider.clone());
+    let total_supply = curve_contract.totalSupply().call().await.unwrap();
+    let virtual_price = contract.get_virtual_price().call().await.unwrap();
 
     println!("xi = {x:#?}");
 
     let xp = vec![
-        x[0].to_big_int() * rates[0] / precision,
-        x[1].to_big_int() * rates[1] / precision,
-        x[2].to_big_int() * rates[2] / precision,
+        x[0].to_big_int() * precision / precisions[0],
+        x[1].to_big_int() * precision / precisions[1],
+        x[2].to_big_int() * precision / precisions[2],
     ];
 
     println!("xp: {xp:#?}");
     let s: BigInt = xp.iter().sum();
     let ann = a * U256::from(n);
 
-    let d_new = get_d_new(ann, s, n, xp.clone());
-    println!("d_new: {}", d_new);
+    let d = get_d_new(ann, s, n, xp.clone());
+    println!("d_new: {}", d);
 
+    let d_exp = virtual_price.to_big_int() * total_supply.to_big_int() / precision;
+    println!("d_exp: {d_exp}");
+
+    println!("diff: {}", d - d_exp);
     let i = 1; // input index
     let j = 0; // output index
     let dx = BigInt::from(1000);
-    let x = xp[i] + (dx * rates[i] / precision);
+    let x = xp[i] + (dx * precision / precisions[i]);
 
-    let y = get_y(i, j, d_new, xp.clone(), x, ann, n);
+    let y = get_y(i, j, d, xp.clone(), x, ann, n);
 
-    let dy = (xp[j] - y - BigInt::ONE) * precision / rates[j];
+    let dy = (xp[j] - y - BigInt::ONE) * precisions[j] /  precision;
 
     let _fee = fee.to_big_int() * dy / fee_denomination;
 
+    println!("_fee: {}", _fee);
+    println!("y: {}", y);
     println!("dy: {}", dy - _fee);
 
     let e_dy = BigInt::from(999844592181965u128);
@@ -148,7 +157,7 @@ fn get_d_new(ann: U256, s: BigInt, n: usize, xp: Vec<BigInt>) -> BigInt {
         d = (((ann * s) + (n * d_p)) * d) / ((ann_1 * d) + (n_1 * d_p));
 
         count = i + 1;
-        if is_abs_le_0(&d, &d_prev) {
+        if is_abs_le_1(&d, &d_prev) {
             break;
         }
     }
@@ -162,7 +171,7 @@ fn get_y(
     j: usize,
     d: BigInt,
     xp_: Vec<BigInt>,
-    x_: BigInt,
+    x: BigInt,
     ann: U256,
     n: usize,
 ) -> BigInt {
@@ -170,11 +179,11 @@ fn get_y(
     let mut s_ = BigInt::default();
     let ann = ann.to_big_int();
     let mut _x = BigInt::default();
-    let annn = ann * BigInt::from(n);
+    let n_big = BigInt::from(n);
 
     for _i in 0..n {
         if _i == i {
-            _x = x_;
+            _x = x;
         } else if _i != j {
             _x = xp_[_i];
         } else {
@@ -184,8 +193,8 @@ fn get_y(
         c = c * d / (_x * BigInt::from(n));
     }
 
-    c = c * d / annn;
-    let b = (s_ + d) / ann;
+    c = c * d / (ann * n_big);
+    let b = s_ + d / ann;
     let mut y_prev;
     let mut y = d;
 
@@ -195,7 +204,7 @@ fn get_y(
         y = ((y * y) + c) / ((y * BigInt::from(2)) + b - d);
         count = i + 1;
 
-        if is_abs_le_0(&y_prev, &y) {
+        if is_abs_le_1(&y_prev, &y) {
             break;
         }
     }
@@ -204,7 +213,7 @@ fn get_y(
     y
 }
 
-fn is_abs_le_0(n1: &BigInt, n2: &BigInt) -> bool {
+fn is_abs_le_1(n1: &BigInt, n2: &BigInt) -> bool {
     if n1 > n2 {
         if n1 - n2 <= BigInt::ONE {
             return true;
